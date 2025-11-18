6213401108bf810ce7eaf6fb4004f7667b52a11a
lewan87 <nawemarchelli@gmail.com>
2025-11-18 19:39:18 -0300

    fixed an api routes bug

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -14,25 +14,29 @@ import upload from "../middleware/upload.js";
 const router = express.Router();
 
 // POST /api/auth/newItem
-router.post("/newItem", upload.array("images"), postItem);
+router.post("/", upload.array("images"), postItem);
+//router.post("/newItem", upload.array("images"), postItem);
 
 // GET /api/auth/items
-router.get("/items", getItems);
+router.get("/", getItems);
+//router.get("/items", getItems);
 
 // GET /api/auth/item/:code
-router.get("/item/:code", getItemByCode);
+router.get("/:code", getItemByCode);
+//router.get("/item/:code", getItemByCode);
 
 // PUT /api/auth/item/:code
-router.put("/item/:code", putItemByCode);
+router.put("/:code", putItemByCode);
+//router.put("/item/:code", putItemByCode);
 
 // DELETE /api/auth/item/:code
-router.delete("/item/:code", deleteItemByCode);
+router.delete("/:code", deleteItemByCode);
+//router.delete("/item/:code", deleteItemByCode);
 
 // POST /api/auth/item/:code/images  -> Agregar imágenes adicionales
-router.post(
-  "/item/:code/images",
-  upload.array("images"), // soporta subida de múltiples archivos
-  addImagesToItem
-);
+router.post("/:code/images", upload.array("images"), addImagesToItem);
+// router.post("/item/:code/images",upload.array("images"), // soporta subida de múltiples archivos
+//   addImagesToItem
+// );
 
 export default router;
